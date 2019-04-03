9ec06994758c753ac1f9a49c7bf8ac6a196e551a
Brokenwind <wangkun6536@163.com>
2019-04-03 03:14:04 -0400

    add: added the operator in table

--- resources/tables/history-location-browser-table.js
+++ resources/tables/history-location-browser-table.js
@@ -1,19 +1,79 @@
-function queryLocationBrowser() {
-    $.ajax({
-        url: '../resources/data/get_location_browsers.json',
-        datatype: "json",//请求页面返回的数据类型
-        type: "GET",
-        contentType: "application/json",//注意请求页面的contentType 要于此处保持一致
-        success: processLocationBrowser, //传递给process函数的data，是由请求页面返回的数据 ,已经被ajax处理成了对象
-        error: function (XMLHttpRequest, textStatus, errorThrown) {
-            alert("error");
-        }
+/**将从ajax获取的数据直接传递到Table中进行显示*/
+function processLocationBrowser() {
+    $("#history_location_browser").bootstrapTable({
+        url: '../resources/data/history_location_browser.json',
+        method: 'GET',                      //请求方式（*）
+        pagination: true,                   //是否显示分页（*）
+        pageList: [10, 25, 50, 100],        //可供选择的每页的行数（*）
+        search: true,                       //是否显示表格搜索
+        clickToSelect: true,                //是否启用点击选中行
+        cardView: false,                    //是否显示详细视图
+        striped: true,                      //是否显示行间隔色
+        sortable: true,                     //是否启用排序
+        sortOrder: "asc",                   //排序方式
+        columns: [
+            {
+                checkbox: true,
+                visible: true               //是否显示复选框
+            }, {
+                field: 'id',
+                title: '编号',
+                sortable: true
+            },
+            {
+                field: 'question',
+                title: '问题',
+                sortable: true
+            },
+            {
+                field: 'answer',
+                title: '答案',
+                sortable: true
+            },
+            {
+                field: 'operator',
+                title: '操作',
+                width: 120,
+                align: 'center',
+                valign: 'middle',
+                // 格式化函数
+                formatter: actionFormatter
+            },
+        ],
+        queryParams: function (params) {
+            //这里的键的名字和控制器的变量名必须一直，这边改动，控制器也需要改成一样的
+            var temp = {
+                rows: params.limit,                         //页面大小
+                page: (params.offset / params.limit) + 1,   //页码
+                sort: params.sort,      //排序列名
+                sortOrder: params.order //排位命令（desc，asc）
+            };
+            return temp;
+        },
+        //数据加载成功
+        onLoadSuccess: function () {
+        },
+        // 数据加载失败
+        onLoadError: function () {
+            alert("数据加载失败！");
+        },
+        // 双击
+        onDblClickRow: function (row, $element) {
+            var opt = row.operator;
+            //EditViewById(id, 'view');
+        },
     });
 }
 
-/**将从ajax获取的数据直接传递到Table中进行显示*/
-function processLocationBrowser(json_data) {
-    $("#history_location_browser").bootstrapTable({
-        url: './get_location_browsers.json'
-    });
+
+//操作栏的格式化
+function actionFormatter(value, row, index) {
+    //alert(JSON.stringify(value))
+    var id = value;
+    var result = "";
+    result += "<a href='javascript:;' class='btn btn-xs green' onclick=\"EditViewById('" + id + "', view='view')\" title='查看'><span class='glyphicon glyphicon-search'></span></a>";
+    result += "<a href='javascript:;' class='btn btn-xs blue' onclick=\"EditViewById('" + id + "')\" title='编辑'><span class='glyphicon glyphicon-pencil'></span></a>";
+    result += "<a href='javascript:;' class='btn btn-xs red' onclick=\"DeleteByIds('" + id + "')\" title='删除'><span class='glyphicon glyphicon-remove'></span></a>";
+
+    return result;
 }
